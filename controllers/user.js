15fcb164ad278e14aa70f8b04b0389bd0bda4e60
liuyuangithub <liuyuancoding@gmail.com>
2018-06-28 21:24:45 +0800

    npm script

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const db = require('./db_helper');
+
 // 1. 展示登陆页面
 
 // 2 处理登陆逻辑
@@ -8,15 +10,19 @@
 
 // 5. 推出
 
+// 渲染登陆页面
 exports.showSignin = (req, res) => {
   res.render('signin.html')
 }
+// 处理登陆页面
 exports.handleSignin = (req, res) => {
   res.send('hello')
 }
+// 渲染注册页面
 exports.showSignup = (req, res) => {
   res.render('signup.html')
 }
+// 处理注册
 exports.handleSignup = (req, res) => {
   res.send('hello')
 }
