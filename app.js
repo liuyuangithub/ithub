a9a0f65908ad2762c50af8e3e914fe0c9764fa63
liuyuangithub <liuyuancoding@gmail.com>
2018-07-02 20:15:59 +0800

    处理服务端注册和登陆逻辑

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ const express = require('express');
 // 导入处理模板的模块
 const expressArtTemplate = require('express-art-template');
 const router = require('./routes/router');
+const bodyParser = require('body-parser');
 const app = express();
-// 注册路由
-app.use(router);
+
 // 处理静态资源
 app.use('/public', express.static('./public'));
 app.use('/node_modules', express.static('./node_modules'))
 // 配置模板引擎
 app.engine('html', expressArtTemplate);
+// 配置body-parser
+// parse application/x-www-form-urlencoded
+// name=zhangsan&age=25  => {name: 'zhangsan', age: 25}
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+// {"name": "zhangsan", "age": "25"}  => {name: 'zhangsan', age: 25}
+// app.use(bodyParser.json())
+
+// 注册路由
+app.use(router);
 // 监听端口
 const PORT = 8080;
 app.listen(PORT, () => {
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const db = require('./db_helper');
-
+const md5 = require('md5');
 // 1. 展示登陆页面
 
 // 2 处理登陆逻辑
@@ -16,7 +16,42 @@ exports.showSignin = (req, res) => {
 }
 // 处理登陆页面
 exports.handleSignin = (req, res) => {
-  res.send('hello')
+  // 2.1 验证用户的输入
+  // TODO
+  // 验证邮箱和密码是否正确
+  console.log(req.body.email)
+  db.query(
+    'select * from `users` where `email`=?',
+    req.body.email,
+    (err, result) => {
+      if (err) {
+        return res.send('服务器内部错误');
+      };
+      // 判断邮箱是否正确
+      if (result.length <= 0) {
+        // { code: 401, msg: '不存在' }
+        return res.json({
+          code: 401,
+          msg: '邮箱地址不存在'
+        })
+      };
+      // 判断密码是否正确
+      const password = md5(req.body.password);
+      if (password !== result[0].password) {
+        return res.json({
+          code: 401,
+          msg: '密码错误'
+        })
+      };
+      // 如果是ajax请求的话， 没办法使用res.redirect()
+      // 成功
+      res.json({
+        code: 200,
+        msg: '登陆成功'
+      })
+
+    }
+  )
 }
 // 渲染注册页面
 exports.showSignup = (req, res) => {
@@ -24,7 +59,76 @@ exports.showSignup = (req, res) => {
 }
 // 处理注册
 exports.handleSignup = (req, res) => {
-  res.send('hello')
+  // 添加数据之前要做数据验证
+  // TODO 验证数据是否输入
+
+  // 验证邮箱是否重复
+  db.query(
+    'select * from `users` where `email`=?',
+    req.body.email,
+    (err, result) => {
+      if (err) {
+        return res.send('服务器内部错误');
+      }
+      if (result.length > 0) {
+        // 数据表中已经存在这个数据
+        res.render('signup.html', {
+          msg: '邮箱已经存在'
+        })
+        return;
+      }
+      // 验证昵称
+      db.query(
+        'select * from `users` where `nickname`=?',
+        req.body.nickname,
+        (err, result) => {
+          if (err) {
+            return res.send('服务器内部错误');
+          }
+          if (result.length > 0) {
+            // 昵称已经存在
+            res.render('signup.html', {
+              msg: '昵称已经存在'
+            })
+            return;
+          }
+          // 邮箱和昵称都不存在， 插入数据
+
+          // console.log(req.body)
+          // 获取post数据，配置body-parser 在app.js中配置
+          req.body.createdAt = new Date();
+          //  对密码进行处理
+          req.body.password = md5(req.body.password);
+
+          // insert into `uesrs` (nickname, pwd) valuse('zs', 45)
+          //插入数据 
+          db.query(
+            'insert into `users` set ?',
+            req.body,
+            (err, result) => {
+              if (err) {
+                return res.send('服务器内部错误');
+              }
+              // console.log(result);
+              if (result.affectedRows === 1) {
+                // 登陆成功
+                // 跳转页面
+                res.redirect('/signin');
+                // res.send('<script>alert("注册成功");location.href="/signin"</script>')
+              } else {
+                res.render('signup.html', {
+                  msg: '注册失败'
+                })
+              }
+            }
+          )
+        }
+      )
+    }
+  )
+
+
+
 }
 exports.handleSignout = (req, res) => {
   res.send('hello')
